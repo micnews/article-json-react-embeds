82521365263d44113b3a17ed45aa655b78db62cb
ellell <lisaovermyr@gmail.com>
2017-04-17 15:58:30 +0200

    Move reactified components from

---
+++ test/fixtures/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs'); // eslint-disable-line
+
+export default {
+  instagramCaption: fs.readFileSync(`${__dirname}/instagram.html`, 'utf8').trim(),
+  instagramWithoutCaption:
+    fs.readFileSync(`${__dirname}/instagram-no-caption.html`, 'utf8').trim(),
+  facebookPost: fs.readFileSync(`${__dirname}/facebook-post.html`, 'utf8').trim(),
+  facebookVideo: fs.readFileSync(`${__dirname}/facebook-video.html`, 'utf8').trim(),
+  facebookPhoto: fs.readFileSync(`${__dirname}/facebook-photo.html`, 'utf8').trim(),
+  tweetNoUser: fs.readFileSync(`${__dirname}/tweet-no-user.html`, 'utf8').trim(),
+  tweetVideo: fs.readFileSync(`${__dirname}/tweet-video.html`, 'utf8').trim(),
+  tumblrPost: fs.readFileSync(`${__dirname}/tumblr-post.html`, 'utf8').trim(),
+};
